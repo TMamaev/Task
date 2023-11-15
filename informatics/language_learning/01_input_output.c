f188debb04d4ee5d9b75d44624bbc24b4dd55266
Replit user <>
2023-11-15 14:49:39 +0000

    Сделал задания по комплекту If, добавил ссылки на задания

--- informatics/language_learning/01_input_output.c
+++ informatics/language_learning/01_input_output.c
@@ -30,7 +30,6 @@ void apple_1()
 }
 
 
-
 void apple_2()
 {
     int n, k, pol;
@@ -43,13 +42,10 @@ void apple_2()
 void MKAD()
 {
     int v;
-  
     int t;
   
     scanf("%d%d", &v, &t);
-    
     printf("%d", ((v * t) % 109 + 109) % 109);
-
 }
 
 
@@ -58,7 +54,6 @@ void last_num()
     int n;
 
     scanf("%d", &n);
-
     printf("%d", n % 10);
 }
 
@@ -66,10 +61,9 @@ void last_num()
 void num_of_tens()
 {
     int n;
-  
-    scanf("%d", &n);
 
-    printf("%d", (n - n % 10) / 10);
+    scanf("%d", &n);
+    printf("%d", n / 10);
 }
 
 
@@ -77,73 +71,64 @@ void num_of_tens2()
 {
     int n;
   
-    int n_without_1;
-  
     scanf("%d", &n);
-    n_without_1 = (n - n % 10);
-    printf("%d", n_without_1 % 100 / 10); 
+    printf("%d", n / 10 % 10);
 }
-
 
 
 void sum_dig()
 {
-  int n;
-  
-  int n_1;
- 
-  int n_10;
-  
-  int n_100; 
-  
-  scanf("%d", &n);
-  n_1 =  n % 100 % 10;
-  n_10 = (n - n % 10) % 100 / 10;
-  n_100 = (n - n % 100) / 100;
-  printf("%d", n_1 + n_10 + n_100);
+    int n;
+    int n_1;
+    int n_10;
+    int n_100;
+    
+    scanf("%d", &n);
+    n_1 =  n % 10;
+    n_10 = n / 10 % 10;
+    n_100 = n / 10 / 10 % 10;
+    printf("%d", n_1 + n_10 + n_100);
 }
-
 
 
 void next_parity()
 {
     int n;
-    int n1;
   
     scanf("%d", &n);
-    n1 = n;
-    n1 %= 2;
-    printf("%d", n + n1 + ((n1 + 2) % 3));
+    printf("%d", n + 2 - n & 1);
 }
-
 
 
 void electric_watch1()
 {
     int n;
+    const int minutes_in_day = 1440;
   
     scanf("%d", &n);
-    n %= 1440;
+    n %= minutes_in_day;
     printf("%d %d", n / 60, n % 60);
 }
 
 
 void electric_watch2()
 {
-    int n; 
-  
+    int n;
+
     scanf("%d", &n);
     n %= 86400;
-    printf("%d:", n / 3600);
-    printf("%d%d:", 0 + (((n % 3600) / 60) / 10), (n % 3600) / 60);
-    printf("%d%d", 0 + ((n % 60) / 10), n % 60);
-}
 
+    // %[flags][width][.precision][length]<type>
+    printf("%d:%02d:%02d", n / 3600, n % 3600 / 60, n % 60);
+}
 
 
 void replace_values()
 {
-    int a, b, c;
+    int a;
+    int b;
+    int c;
+
     scanf("%d%d", &a, &b);
     c = a;
     a = b;
@@ -152,19 +137,19 @@ void replace_values()
 }
 
 
-
+//////////////////////////////////////////
 void lesson_time()
 {
-  int les;
-
-  int time15;
-
-  int time5;
-
-  scanf("%d", les);
-  time5 = les / 2 + les % 2;
-  time15 = les / 2;
-  printf("%d %d", (540 + 5 * time5  + 15 * time15) / 60, (540 + 5 * time5 + 15 * time15) % 60);
+    int les;
+    int time15;
+    int time5;
+    
+    scanf("%d", &les);
+    time5 = les / 2 + les % 2;
+    time15 = les / 2;
+    printf("%d %d",
+        (540 + 5 * time5  + 15 * time15) / 60,
+        (540 + 5 * time5 + 15 * time15) % 60);
 }
 
 
@@ -181,10 +166,11 @@ void price()
 }
 
 
-
 void apple_3()
 {
-    int n, k;
+    int n;
+    int k;
+
     scanf("%d%d", &n, &k);
     printf("%d", (n - k % n) % n);
 }
@@ -192,7 +178,10 @@ void apple_3()
 
 void snail()
 {
-    int a, b, h;
+    int a;
+    int b;
+    int h;
+
     scanf("%d%d%d", &h, &a, &b);
     printf("%d", (h - b) / (a - b) + ((h - b) % (a - b)) % 2);
 }
@@ -200,7 +189,9 @@ void snail()
 
 void check_divisibility()
 {
-    int n, m;
+    int n;
+    int m;
+    
     scanf("%d%d", &n, &m);
     printf("%d", n % m + 1);
 }
