b907d616d2cfd5a114ca980b2e7f138b53d3afda
TMamaev <timurmamaev1705@gmail.com>
2023-12-16 00:20:03 +0300

    Solve 6 tasks

--- informatics/language_learning/04_array.c
+++ informatics/language_learning/04_array.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 // https://informatics.msk.ru/mod/statements/view.php?id=208#1
 void even()
@@ -16,12 +17,107 @@ void even()
             printf("%d ", A[i]);
 }
 
+void even_nums()
+{
+    int n;
+    int num;
+
+    scanf("%d", &n);
+    int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        A[i] = num;
+    }
+    for (int i = 0; i < n; i++)
+        if (A[i] % 2 == 0)
+            printf("%d ", A[i]);
+}
+
+void num_pos()
+{
+    int n;
+    int num;
+    int sum = 0;
+
+    scanf("%d", &n);
+    int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        A[i] = num;
+    }
+    for (int i = 0; i < n; i++)
+        if (A[i] > 0)
+            sum++;
+    printf("%d", sum);
+}
+
+void more_prev_num()
+{
+    int n;
+    int num;
+    int sum = 0;
+
+    scanf("%d", &n);
+    int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        A[i] = num;
+    }
+    for (int i = 1; i < n; i++)
+        if (A[i] > A[i - 1])
+            sum++;
+    printf("%d", sum);
+}
+
+void one_sign()
+{
+    bool check = false;
+    int n;
+    int num;
+
+    scanf("%d", &n);
+    int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        A[i] = num;
+    }
+    for (int i = 1; i < n; i++)
+        if ((A[i] > 0 && A[i - 1] > 0) || (A[i] < 0 && A[i - 1] < 0))
+            check = true;
+    if (check)
+        printf("YES");
+    else 
+        printf("NO");
+}
+
+void small_big_small()
+{
+    int sum = 0;
+    int n;
+    int num;
+
+    scanf("%d", &n);
+    int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        A[i] = num;
+    }
+    for (int i = 2; i < n; i++)
+        if (A[i] < A[i - 1] && A[i - 1] > A[i - 2])
+            sum++;
+    printf("%d", sum);
+}
 
 #ifdef MAIN
 
 int main()
 {
-    even();
+    small_big_small();
     return 0;
 }
 
