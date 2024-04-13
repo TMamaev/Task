a3844b82fb813402142e0fc1e005f79c65c219c1
Timur <timurmamaev1705@gmail.com>
2024-04-13 11:48:44 +0200

    Refactoring

--- Leetcode/Palindrome.c
+++ Leetcode/Palindrome.c
@@ -5,15 +5,13 @@ int isPalindrome(int n)
 {
     long long res = 0;
     long long n1 = n;
-    while(n1 != 0)
+
+    while (n1 != 0)
     {
         res = res * 10 + n1 % 10;
         n1 /= 10;
     }
-    if (res == n && res >= 0)
-        return 1;
-    else
-        return 0;
+    return res == n && res >= 0;
 }
 
 int main()
@@ -21,11 +19,7 @@ int main()
     int n;
 
     scanf("%d", &n);
-    if (n < 0)
-        printf("false");
-    else if (n == 0)
-        printf("true");
-    else if (isPalindrome(n))
+    if (isPalindrome(n))
         printf("true");
     else
         printf("false");
