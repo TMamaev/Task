025ebda4be655fc0a39bda82e13586cc04aac7bf
Timur <timurmamaev1705@gmail.com>
2024-05-16 10:51:40 +0200

    Solve problems

--- informatics/language_learning/06_Data_type.c
+++ informatics/language_learning/06_Data_type.c
@@ -7,28 +7,27 @@ void rabbit()
     int n, m;
 
     scanf("%d%d", &n, &m);
-    if (n < m)
-    {
-        printf("%d", m % n + m / n);
-    }
-    else
-    {
-        printf("%d", 1);
-    }
+    printf("%d", m / n + (m % n != 0));
 }
 
 // https://informatics.msk.ru/mod/statements/view.php?id=1120&chapterid=1466#1
 void find_sum()
 {
     long long n;
-    
+    long long s = 0;
+    long long sign = 0;
+
     scanf("%lli", &n);
-    if (n > 0)
-        printf("%lli", ((1 + n) * n) / 2);
-    else if (n < 0)
-        printf("%lli", (n + 1) * (llabs(n) + 2) / 2);
-    else 
-        printf("1");
+    if (n < 0)
+        ++sign;
+    n = llabs(n);
+    if (n % 2 == 0)
+        s = n / 2 * (n + 1);
+    else
+        s = (n + 1) / 2 * n;
+    if (sign)
+        s = -s + 1;
+    printf("%lli", s);
 }
 
 // https://informatics.msk.ru/mod/statements/view.php?id=1120&chapterid=1475#1
@@ -46,8 +45,7 @@ void time_2()
     int d;
     
     scanf("%d", &d);
-    d %= 360;
-    printf("It is %d hours %d minutes.", d / 30, d % 30);
+    printf("It is %d hours %d minutes.", d / 30, d % 30 / 6);
 }
 
 // https://informatics.msk.ru/mod/statements/view.php?id=1120&chapterid=1479#1
@@ -71,4 +69,9 @@ void seconds()
     s1 = s1 + m1 * 60 + h1 * 3600;
     s2 = s2 + m2 * 60 + h2 * 3600;
     printf("%d", s2 - s1);
+}
+
+int main()
+{
+    rabbit();
 }
