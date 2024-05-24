e74039c5250f951b08e5581947e868b772081547
Timur <timurmamaev1705@gmail.com>
2024-05-25 01:05:21 +0200

    Remake problems

--- informatics/language_learning/07_Structur.c
+++ informatics/language_learning/07_Structur.c
@@ -10,12 +10,14 @@ struct point
 // https://informatics.msk.ru/mod/statements/view.php?id=279&chapterid=322#1
 void point_on_coordinate()
 {
-    int n, max_x = 0, max_y = 0;
+    int n;
+    int max_x = 0;
+    int max_y = 0;
+    struct point p = {p.x, p.y};
     
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        struct point p = {p.x, p.y};
         scanf("%d%d", &p.x, &p.y);
         
         if (abs(p.x) + abs(p.y) > abs(max_x) + abs(max_y))
@@ -31,12 +33,13 @@ void point_on_coordinate()
 void mid_num()
 {
     int n;
-    double mid_x = 0, mid_y = 0;
+    double mid_x = 0;
+    int mid_y = 0;
+    struct point p = {p.x, p.y};
     
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        struct point p = {p.x, p.y};
         scanf("%d%d", &p.x, &p.y);
         mid_x += p.x;
         mid_y += p.y;
@@ -46,43 +49,44 @@ void mid_num()
 
 struct student
 {
-  char surname;
-  char name;
-  int m1;
-  int m2;
-  int m3;
+    char surname;
+    char name;
+    int mark1;
+    int mark2;
+    int mark3;
 };
 
 // https://informatics.msk.ru/mod/statements/view.php?id=279&chapterid=328#1
 void marks()
 {
     int n;
-    double mid_m1 = 0, mid_m2 = 0, mid_m3 = 0;
-    
+    double mid_mark1 = 0;
+    int mid_mark2 = 0;
+    int mid_mark3 = 0;
+    struct student s = {s.surname, s.name, s.mark1, s.mark2, s.mark3};
+
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        struct student s = {s.surname, s.name, s.m1, s.m2, s.m3};
-        scanf("%s%s%d%d%d", &s.surname, &s.name, &s.m1, &s.m2, &s.m3);
-        mid_m1 += s.m1;
-        mid_m2 += s.m2;
-        mid_m3 += s.m3;
+        scanf("%s%s%d%d%d", &s.surname, &s.name, &s.mark1, &s.mark2, &s.mark3);
+        mid_mark1 += s.mark1;
+        mid_mark2 += s.mark2;
+        mid_mark3 += s.mark3;
     }
-    printf("%f %f %f", mid_m1 / n, mid_m2 / n, mid_m3 / n);
+    printf("%f %f %f", mid_mark1 / n, mid_mark2 / n, mid_mark3 / n);
 }
 
 // https://informatics.msk.ru/mod/statements/view.php?id=279&chapterid=329#1
 void mark_3()
 {
     int n;
+    struct student s = {s.surname, s.name, s.mark1, s.mark2, s.mark3};
     
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        struct student s = {s.surname, s.name, s.m1, s.m2, s.m3};
-        scanf("%s%s%d%d%d", &s.surname, &s.name, &s.m1, &s.m2, &s.m3);
-        
-        if (s.m1 > 3 && s.m2 > 3 && s.m3 > 3)
+        scanf("%s%s%d%d%d", &s.surname, &s.name, &s.mark1, &s.mark2, &s.mark3);
+        if (s.mark1 > 3 && s.mark2 > 3 && s.mark3 > 3)
             printf("%s %s", s.surname, s.name);
     }
 }
@@ -92,19 +96,16 @@ void best_marks()
 {
     int n;
     double max_mid = 0;
+    struct student s = {s.surname, s.name, s.mark1, s.mark2, s.mark3};
     
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        struct student s = {s.surname, s.name, s.m1, s.m2, s.m3};
-        scanf("%s%s%d%d%d", s.surname, s.name, &s.m1, &s.m2, &s.m3);
-        
-        if ((s.m1 + s.m2 + s.m3) / 3 > max_mid)
+        scanf("%s%s%d%d%d", s.surname, s.name, &s.mark1, &s.mark2, &s.mark3);
+        if ((s.mark1 + s.mark2 + s.mark3) / 3 > max_mid)
         {
-            max_mid = (s.m1 + s.m2 + s.m3) / 3;
+            max_mid = (s.mark1 + s.mark2 + s.mark3) / 3;
             printf("%s %s", s.surname, s.name);
         }
     }
 }
-
-
