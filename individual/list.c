69d0138b5b14df9617a172b881e4bf069ac4bafb
Timur <timurmamaev1705@gmail.com>
2024-08-18 14:02:47 +0200

    Did merge two lists, will continue tomorrow

--- individual/list.c
+++ individual/list.c
@@ -93,6 +93,33 @@ void ft_lstadd_back(t_list **lst, t_list *new)
     (*lst)->next = new;
 }
 
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode *copy_next1 = list1->next;
+    struct ListNode *copy_next2 = list2->next;
+    while (list1 != NULL || list2 != NULL)
+    {
+        if (list1->next != NULL)
+            copy_next1 = list1->next;
+        else 
+            copy_next1 = NULL;
+        if (list2->next != NULL)
+            copy_next2 = list2->next;
+        else 
+            copy_next2 = NULL;
+        if (list1->val < list2->val)
+        {
+            list1->next = list2;
+            list1 = copy_next1;
+        }
+        else
+        {
+            list2->next = list1;
+            list2 = copy_next2;
+        }
+    }
+    return list2;
+}
+
 int main()
 {
     t_list *new;
