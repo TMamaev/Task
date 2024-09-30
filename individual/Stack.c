695b3a4441c8c612155a9447d2fcb891b3fc723a
Timur <timurmamaev1705@gmail.com>
2024-09-30 21:46:38 +0200

    Add Palindrome Linked List

--- individual/Stack.c
+++ individual/Stack.c
@@ -5,7 +5,7 @@
 
 typedef struct list_stack
 {
-    char data;
+    int data;
     struct list_stack *next;
 } t_stack;
 
@@ -21,19 +21,7 @@ void push(t_stack** list, char n)
     *list = new;
 }
 
-void pop(t_stack** list)
-{
-    t_stack *copy_head;
-
-    if (!list || !*list)
-        return ;
-    
-    copy_head = *list;
-    *list = (*list)->next;
-    free(copy_head);
-}
-
-int main()
+int valid_paren()
 {   
     char s[] = "([])(){}";
 
@@ -52,7 +40,7 @@ int main()
         if (s[i] == '(' || s[i] == '[' || s[i] == '{')
         {
             push(&list, s[i]);
-            printf("%c", list->data);
+            // printf("%c", list->data);
         }
         else
         {
@@ -69,5 +57,84 @@ int main()
         }
     }
     printf("true");
+    return 0;
+}
+
+typedef struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} t_n;
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    if (root == NULL)
+        return 0;
+    returnSize++;
+    preorderTraversal(root->right, returnSize);
+    preorderTraversal(root->left, returnSize);
+    return 0;
+}
+
+void pop(t_stack** list)
+{
+    t_stack *copy_head;
+
+    if (!list || !*list)
+        return ;
+    
+    copy_head = *list;
+    *list = (*list)->next;
+    free(copy_head);
+}
+
+int isPalindrome(t_stack* head) {
+    int n = 0;
+    int check = 0;
+    struct ListNode *h_c = head;
+    while (head != NULL)
+    {
+        head = head->next;
+        n++;
+    }
+    head = h_c;
+    for (int i = 1; i <= n;)
+    {
+
+        struct ListNode *h_c = head;
+        for (int k = i; k < n; k++) 
+        {
+            head = head->next;
+        }
+        int num;
+        num = head->data;
+        head = h_c;
+        if (head->data != num)
+        {
+            return 0;
+        }
+        i++;
+        n--;
+        head = head->next;
+    }
+    return 1;
+}
+
+int main()
+{   
+    t_stack *head = malloc(sizeof(t_stack));
+    head->data = 1;
+    t_stack *head1 = malloc(sizeof(t_stack));
+    head1->data = 2;
+    t_stack *head2 = malloc(sizeof(t_stack));
+    head2->data = 1;
+    head->next = head1;
+    head1->next = head2;
+    head2->next = NULL;
+    
+    if (isPalindrome(head))
+        printf("true");
+    else    
+        printf("false");
+
     return 0;
 }
