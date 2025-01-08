d68cede89da8c8a943e67cf55ffb365f57049d86
Timur <timurmamaev1705@gmail.com>
2025-01-08 19:43:52 +0100

    Add Grafe.c

--- Leetcode/Grafe.c
+++ Leetcode/Grafe.c
@@ -21,63 +21,86 @@ void enqueue(t_queue** head, int x, int y, int move)
 
 void dequeue(t_queue** head)
 {
-    t_queue *copy = *head;
-    t_queue *copy_next = NULL;
+    t_queue *walker = NULL;
+    t_queue *copy_del = NULL;
+    t_queue *prelast_item = NULL;
 
-    while ((*head)->next != NULL)
+    if (!head || !*head)
+        return ;
+    walker = *head;
+    while (walker->next != NULL)
     {
-        copy_next = *head;
-        *head = (*head)->next;
+        prelast_item = walker;
+        walker = walker->next;
     }
-    t_queue *copy_del = *head;
+    if (prelast_item)
+    {
+        copy_del = prelast_item->next;
+        prelast_item->next = NULL;
+    }
+    else
+    {
+        copy_del = *head;
+        *head = NULL;
+    }
     free(copy_del);
-    *head = copy_next;
-    (*head)->next = NULL;
-    *head = copy;
-    
+}
+
+t_queue* top(t_queue* head)
+{
+    if (head == NULL)
+        return NULL;
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+    return head;
+}
+
+void add_cell(char** maze, int rows, int cols,
+                  t_queue** q, int x, int y, int move)
+{
+    if (x < cols && x >= 0 && y < rows && y >= 0 && maze[y][x] == '.')
+    {
+        enqueue(q, x, y, move);
+        maze[y][x] = '*';
+    }
 }
 
 int nearestExit(char** maze, int mazeSize, int* mazeColSize,
                 int* entrance, int entranceSize) {
     int x = entrance[1];
     int y = entrance[0];
-    t_queue *head;
-    enqueue(&head, x, y, 0);
-    while (head->y != 0 && head->x != 0 && head->y !=mazeSize && head->x != *mazeColSize)
-    {
-        while (head->next != NULL)
-        {
-            head = head->next;
-        }
+    int cols = *mazeColSize;
+    int rows = mazeSize;
+    t_queue *q;
 
-        x = head->x;
-        y = head->y;
-        if (maze[y][x + 1] == ".")
-        {
-            enqueue(&head, x + 1, y, head->move + 1);
-        }
-        if (maze[y][x - 1] == ".")
-        {
-            enqueue(&head, x - 1, y, head->move + 1);
-        }
-        if (maze[y + 1][x] == ".")
-        {
-            enqueue(&head, x, y + 1, head->move + 1);
-        }
-        if (maze[y - 1][x] == ".")
-        {
-            enqueue(&head, x, y - 1, head->move + 1);
-        }
-        if (head != NULL)
-            dequeue(&head);
-        else return -1;
-    }
-    int min  = 9999999;
-    while (head != NULL)
+    maze[y][x] = '*';
+    add_cell(maze, rows, cols, &q, x + 1, y, 1);
+    add_cell(maze, rows, cols, &q, x - 1, y, 1);
+    add_cell(maze, rows, cols, &q, x, y + 1, 1);
+    add_cell(maze, rows, cols, &q, x, y - 1, 1);
+
+    while (q)
     {
-        if (head->move < min) 
-            min = head->move;
-        head = head->next;
+        t_queue *q_top = top(q);
+        x = q_top->x;
+        y = q_top->y;
+        if (q_top->y == 0 || q_top->x == 0 ||
+            q_top->y == rows - 1 || q_top->x == cols - 1)
+            return q_top->move;
+
+        add_cell(maze, rows, cols, &q, x + 1, y, q_top->move + 1);
+        add_cell(maze, rows, cols, &q, x - 1, y, q_top->move + 1);
+        add_cell(maze, rows, cols, &q, x, y + 1, q_top->move + 1);
+        add_cell(maze, rows, cols, &q, x, y - 1, q_top->move + 1);
+        dequeue(&q);
     }
-    return min;
+    return -1;
 }
+// 776 54321 ->
+// [+!+++++]
+// [+1+...+]
+// [+2+6+.+]
+// [+34567+]
+// [++++7+.]
