8f94d33673089f738190c1c74f77f1904d470fe7
Timur <timurmamaev1705@gmail.com>
2025-04-20 09:52:27 +0200

    Have to check at Leetcode

--- Leetcode/3.LongestSubstring.c
+++ Leetcode/3.LongestSubstring.c
@@ -1,68 +1,40 @@
-int Size(char* s)
-{
-    int size = 0;
-    while(s[size])
-    {
-        size++;
-    }
-    return size;
-}
-
-void makeone(int* ASCII, int left, int right, char *s)
-{
-    for (; left <= right; left ++)
-    {
-       int l = (int)s[left];
-        ASCII[l] = 1;
-    }
-}
+#include <string.h>
 
 int lengthOfLongestSubstring(char* s) 
 {
-    int max = 0;
-    int size = Size(s);
+    int max = 1;
+    int size = strlen(s);
     int left = 0;
     int right = 0;
-    int ASCII[127] = {0};
+    int ASCII[127];
+    memset(ASCII, 0xFF, 127 * sizeof(int));
     int r_ASCII_pos;
-    int l_ASCII_pos = (int)s[left];
-    int change = 0;
 
-    if (strcmp(s, "") == 0)
+    if (size == 0)
         return 0;
-
+    
     for (; right < size; right++)
     {
-        printf("%d %d %d\n", left, right, max);
         r_ASCII_pos = (int)s[right];
-        if (ASCII[r_ASCII_pos] == 0)
-        {
-            ASCII[r_ASCII_pos] = 1;
-        }
-        else
+        if (ASCII[r_ASCII_pos] != -1 || ASCII[r_ASCII_pos] >= left)
         {
             if (right - left > max)
-                max = right - left;
-            ASCII[r_ASCII_pos] = 2;
-            for (;  r_ASCII_pos != l_ASCII_pos; left++)
             {
-                ASCII[l_ASCII_pos] = 0;
-                l_ASCII_pos = (int)s[left];
-                change++;
+                max = right - left;
             }
-            if (change == 0)
-                left++;
-            change = 0;
-            makeone(ASCII, left, right, s);            
+            left = ASCII[r_ASCII_pos] + 1;
         }
+        ASCII[r_ASCII_pos] = right;
     }
-    if (ASCII[r_ASCII_pos] == 1 && right - left > max)
-        return right-left;
-        
-    if (max == 0 && l_ASCII_pos > 0)
-        return 1;
+    if (right - left > max)
+    {
+        max = right - left;
+    }
     return max;
 }
+
+// abba
+//   ||
 
 // 1. Что делать, если встретили символ повторно? -
 // Рассчитываем длину подстроки до right (не включительно) -> right - left ? max
@@ -77,4 +49,4 @@ int lengthOfLongestSubstring(char* s)
 // 
 
 // abdebcccbab
-// |   | 
+// |  | 
