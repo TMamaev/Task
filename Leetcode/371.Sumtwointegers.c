1432e58bc076f6aaeda50bb9362073ca9c9499ab
Timur <timurmamaev1705@gmail.com>
2025-05-15 20:38:52 +0200

    finish task

--- Leetcode/371.Sumtwointegers.c
+++ Leetcode/371.Sumtwointegers.c
@@ -1,32 +1,13 @@
+// https://leetcode.com/problems/sum-of-two-integers/submissions/1634946018/
 int getSum(int a, int b) {
-    int sum, shift;
-    int check = 1;
-    
-    if (a < 0 && b < 0)
+    int sum;
+    unsigned shift;
+    int check = 1;  
+    while (b) 
     {
-        a *= -1;
-        b *= -1;
-        check *= -1;
-
-        while (b) 
-        {
         shift = a & b;
         a = a ^ b;
         b = shift << 1;
-        }
-    }
-    else if (a > 0 && b > 0)
-    {   
-        while (b) 
-        {
-        shift = a & b;
-        a = a ^ b;
-        b = shift << 1;
-        }
-    }
-    else
-    {
-
     }
     return a * check;
 }
