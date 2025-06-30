bfedeecf68c4e8296ede27ba6480a6df564f4da7
Timur <timurmamaev1705@gmail.com>
2025-06-30 18:59:48 +0200

    Did list.h and refact list.c

--- individual/list.c
+++ individual/list.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <list.h>
 
 typedef struct list_item
 {
-    int data;
+    void* data;
     struct list_item *next;
 } t_list;
 
@@ -21,8 +22,6 @@ int lst_length(struct list_item *head)
     return length;
 }
 
-// #define NULL ((void*)0)
-
 t_list *lst_last(t_list *head)
 {
     if (head == NULL)
@@ -34,52 +33,31 @@ t_list *lst_last(t_list *head)
     return head;
 }
 
-// Создать новый элемент списка с содержимым контент
-t_list *ft_lstnew(int content)
+void ft_lstadd_front(t_list **head, void* data)
 {
     t_list *new = malloc(sizeof(t_list));
-
-    if (new)
-        new->data = content;
-    return new;
-}
-
-void ft_lstadd_front(t_list **head, t_list *new)
-{
+    new->data = data;
     if (!head || !new)
         return ;
     new->next = *head;
     *head = new;
 }
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
-{
-    del(lst);
-    free(lst);
-}
-
-void ft_lstclear(t_list **lst, void (*del)(void *))
+void ft_lstclear(t_list **lst, void (*func)(void *))
 {
     while((*lst)->next != NULL)
     {
-        del(lst);
-        free(*lst);
+        func((*lst)->data);
+        t_list *copy_head = (*lst);
+        free(copy_head);
         *lst = (*lst)->next;
     }
 }
 
-void ft_lstiter(t_list *lst, void (*f)(void *))
+void ft_lstadd_back(t_list **lst, void* data)
 {
-    while (lst->next != NULL)
-    {
-        f(lst);
-        lst = lst->next;
-    }
-}
-
-// Добавить новый элемент в конец списка
-void ft_lstadd_back(t_list **lst, t_list *new)
-{
+    t_list *new = malloc(sizeof(t_list));
+    new->data = data;
     if (!new)
         return ;
     if (*lst == NULL)
@@ -93,78 +71,7 @@ void ft_lstadd_back(t_list **lst, t_list *new)
     }
     (*lst)->next = new;
 }
- 
-struct List_item* mergeTwoLists(t_list *list1, t_list *list2) {
-    t_list *copy_head;
-    t_list *list_ans;
-    copy_head = list_ans;
-    while (list1 != NULL && list2 != NULL)
-    {
-        if (list1 == NULL)
-        {
-            list_ans->data = list2->data;
-            list1 = list2->next;
-        }
-        else if (list2 == NULL)
-        {
-            list_ans->data = list1->data;
-            list1 = list1->next;
-        }
-        else if (list1->data <= list2->data)
-        {
-            list_ans->data = list1->data;
-            list1 = list1->next;
-        }
-        else
-        {
-            list_ans->data = list2->data;
-            list1 = list2->next;
-        }
-        t_list *copy_next = list_ans->next;
-        t_list *list_ans;
-        copy_next = list_ans;
-    }
-    
-    return copy_head;
-}
 
-struct List_item* removeNthFromEnd(t_list *head, int n) {
-    t_list *copy_head = head;
-    t_list *copy_next = head->next;
-    int l = 0;
-
-    while (head != NULL)
-    {
-        head = head->next;
-        l++;
-    }
-    head = copy_head;
-    if (l == n)
-    {
-        free(head);
-        return copy_next;
-    }
-    l = l - n;
-    while (l > 1)
-    {
-        l--;
-        head = head->next;
-    }
-    if (n != 1)
-    {
-        copy_next = (head->next)->next;
-    }
-    free(head->next);
-    if (n != 1)
-    {
-        head->next = copy_next;
-    }
-    else
-    {
-        head->next = NULL;
-    }
-    return copy_head;
-}
 
 int main()
 {   
